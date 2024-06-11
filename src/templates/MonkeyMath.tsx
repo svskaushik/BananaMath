8c0e0165d7f897da70c760cefedfd3701fbdf312
svskaushik <shaurya9702@gmail.com>
2024-06-11 17:41:28 -0600

    Add basic timer

--- src/templates/MonkeyMath.tsx
+++ src/templates/MonkeyMath.tsx
@@ -1,5 +1,5 @@
 import type { ChangeEvent, FormEvent } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 // Function to generate random numbers between min and max (inclusive)
 function getRandomNumber(min: number, max: number): number {
@@ -17,6 +17,9 @@ export default function MonkeyMath() {
   const [message, setMessage] = useState<string>('');
   const [result, setResult] = useState<number | null>(null);
   const [isCorrect, setIsCorrect] = useState<boolean | null>(null);
+  const [timerEnded, setTimerEnded] = useState<boolean>(false);
+  const [timeLeft, setTimeLeft] = useState<number>(10); // Start the timer at 10 seconds
+  const [timerStarted, setTimerStarted] = useState<boolean>(false);
 
   // Function to prompt the user with a math problem
   function promptMathProblem() {
@@ -68,6 +71,26 @@ export default function MonkeyMath() {
     setTimeout(promptMathProblem, 300); // Wait 1 second before prompting a new problem
   }
 
+  // Populate the first problem when the component mounts
+  useEffect(() => {
+    promptMathProblem();
+  }, []);
+
+  // Update the timer every second once it has started
+  useEffect(() => {
+    if (timerStarted && timeLeft > 0) {
+      const timer = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000); // Decrease the time left every second
+
+      // Clean up the timer when the component unmounts
+      return () => clearTimeout(timer);
+    }
+    if (timeLeft === 0) {
+      setTimerEnded(true);
+    }
+  }, [timerStarted, timeLeft]);
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between bg-zinc-800 p-4 text-zinc-200">
       <header className="mb-8 flex w-full items-center justify-between">
@@ -85,54 +108,67 @@ export default function MonkeyMath() {
           <button className={TEXT_CLASS}>Settings</button>
         </div>
       </header>
-      <div className="flex min-h-full w-full max-w-3xl flex-col items-center rounded-lg bg-zinc-700/30 p-6 shadow-lg">
-        {/* <div className="flex justify-between items-center mb-4">
-      <div className="flex space-x-2">
-        <button className={BUTTON_CLASS}>Addition</button>
-        <button className={BUTTON_CLASS}>Subtraction</button>
-        <button className={BUTTON_CLASS}>Multiplication</button>
-        <button className={BUTTON_CLASS}>Division</button>
-      </div>
-      <div className="flex space-x-2">
-        <button className={BUTTON_CLASS}>Easy</button>
-        <button className={BUTTON_CLASS}>Medium</button>
-        <button className={BUTTON_CLASS}>Hard</button>
-      </div>
-    </div> */}
-        <div className="mb-6 text-center">
-          <p className="mb-5 text-2xl">{problem}</p>
+      <div className="flex min-h-full w-full flex-col items-center">
+        <div className="mb-2 flex items-center justify-center">
+          {/* Display the time left */}
+          <div className="mb-6 text-center">
+            {/* <p className="mb-5 text-2xl">{problem}</p> */}
+            <p>{timeLeft}</p>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <form onSubmit={handleSubmit}>
-            <input
-              value={userInput}
-              onChange={(e: ChangeEvent<HTMLInputElement>) =>
-                setUserInput(e.target.value)
-              }
-              type="number"
-              className={`flash mb-4 w-80 rounded-md border-0 bg-zinc-600/20 p-2 text-center focus:outline-none focus:ring-4 focus:ring-zinc-400/20 ${isCorrect === true ? 'animate-flash-green' : isCorrect === false ? 'animate-flash-red' : ''}`}
-            />
-            <div className="mb-6 flex space-x-10">
-              <button
-                type="submit"
-                // onClick={checkAnswer}
-                className="rounded bg-zinc-600/20 px-4 py-2 font-bold text-white hover:bg-opacity-30"
-              >
-                Check Answer
-              </button>
-              <button
-                type="button"
-                onClick={promptMathProblem}
-                className="rounded bg-zinc-600/20 px-4 py-2 font-bold text-white hover:bg-opacity-30"
-              >
-                New Problem
-              </button>
-            </div>
-          </form>
+        <div className="flex min-h-full w-full max-w-3xl flex-col items-center rounded-lg bg-zinc-700/30 p-6 shadow-lg">
+          {/* <div className="flex justify-between items-center mb-4">
+        <div className="flex space-x-2">
+          <button className={BUTTON_CLASS}>Addition</button>
+          <button className={BUTTON_CLASS}>Subtraction</button>
+          <button className={BUTTON_CLASS}>Multiplication</button>
+          <button className={BUTTON_CLASS}>Division</button>
         </div>
-        {/* <div className="mt-4 flex justify-center">
-          <button className={SUBMIT_BUTTON_CLASS}>Submit</button>
-        </div> */}
+        <div className="flex space-x-2">
+          <button className={BUTTON_CLASS}>Easy</button>
+          <button className={BUTTON_CLASS}>Medium</button>
+          <button className={BUTTON_CLASS}>Hard</button>
+        </div>
+            </div> */}
+          <div className="mb-6 text-center">
+            <p className="mb-5 text-2xl">{problem}</p>
+          </div>
+          <div className="flex justify-center">
+            <form onSubmit={handleSubmit}>
+              <input
+                value={userInput}
+                onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                  if (!timerStarted) {
+                    setTimerStarted(true);
+                  }
+                  setUserInput(e.target.value);
+                }}
+                type="number"
+                className={`flash mb-4 w-80 rounded-md border-0 bg-zinc-600/20 p-2 text-center focus:outline-none focus:ring-4 focus:ring-zinc-400/20 ${isCorrect === true ? 'animate-flash-green' : isCorrect === false ? 'animate-flash-red' : ''}`}
+                disabled={timerEnded}
+              />
+              <div className="mb-6 flex space-x-10">
+                <button
+                  type="submit"
+                  // onClick={checkAnswer}
+                  className="rounded bg-zinc-600/20 px-4 py-2 font-bold text-white hover:bg-opacity-30"
+                >
+                  Check Answer
+                </button>
+                <button
+                  type="button"
+                  onClick={promptMathProblem}
+                  className="rounded bg-zinc-600/20 px-4 py-2 font-bold text-white hover:bg-opacity-30"
+                >
+                  New Problem
+                </button>
+              </div>
+            </form>
+          </div>
+          {/* <div className="mt-4 flex justify-center">
+            <button className={SUBMIT_BUTTON_CLASS}>Submit</button>
+          </div> */}
+        </div>
       </div>
       <footer className="mt-8 flex w-full items-center justify-between text-sm text-zinc-400">
         <div className="flex space-x-2">
